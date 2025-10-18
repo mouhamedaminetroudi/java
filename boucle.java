5659c7abd666eb93c2313db37fec1e193ad10085
AMIN TROUDI <troudima69@gmail.com>
2025-10-18 01:45:11 +0100

    boucle.java

---
+++ boucle.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class boucle {
+    public static void main(String[]args){
+        Scanner Sc=new Scanner(System.in);
+/*         System.out.println("enter any number");
+        int x=Sc.nextInt();
+        int f=1;
+        for(int i= x ;i> 1 ;i--){
+            f=f*i ;
+
+        }
+        System.out.println("factoriel="+f); */
+/*         int even=0 ;
+        int odd=0;
+        int i=0 ;
+        while(i<10){
+            System.out.println("give me number");
+            int x=Sc.nextInt();
+            i++ ;
+            if(x % 2==0){
+                even+=x;
+            }else{
+                odd+=x;
+            }
+        }
+        System.out.println("some even number="+even);
+        System.out.println("some odd number="+odd); */
+
+
+/*         float x=1f ;
+        System.out.println("saisir un entier");
+        int n=Sc.nextInt();
+        for(int i=1;i<=n;i++){
+            x+=1.0/i;
+        }
+        System.out.println(x); */
+        System.out.println("Test your self 😁");
+        System.out.println("What is '5+10/2*10=?'🫣");
+        float a ;
+        for(int i=1;i<=3;i++){
+            a=Sc.nextFloat();
+            if(a==55){
+                System.out.println("sahit kasah sahbi😎😎");
+                break;
+                
+            }
+            else{
+                System.out.println("hawel mara o5ra😒");
+            }
+        }
+
+
+    }
+}
